f7dc1937841b6fdc38d124eca480d851db20d761
Georgi Mateev <gmmateev92@abv.bg>
2014-04-11 09:12:40 +0300

    Enabled Angular url Html5 mode, added amazon service test method

--- app.js
+++ app.js
@@ -25,13 +25,27 @@ app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'www')));
 
+//Ensure that all url are redirected to index.html/#![path].
+//This will allow Angular to handle all url paths in Html5 mode and fallback to hashbang mode if needed.
+app.use(function(req, res){
+    return res.redirect(req.protocol + '://' + req.get('Host') + '/#!' + req.url);
+});
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-//app.get('/', routes.index);
-app.get('/users', user.list);
+app.get("/api/amazon/test", function(req, res){
+    var service = require("./scripts/amazonService");
+    var client =  service.initialize("key", "secretKey", "associateTag");
+    client.searchItems({SearchIndex: "Books", Keywords: "Javascript"}, function(err, result){
+        var body = err || result;
+
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(body));
+    });
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- www/scripts/app.js
+++ www/scripts/app.js
@@ -1,18 +1,25 @@
 define(["angular", "controllers/loader", "ngRoute", "uiRouter"],
- function (angular) {
+    function (angular) {
      var module = angular.module('giftMeUp', ['giftMeUp.controllers', 'ngRoute', 'ui.router'])
-      .config(function ($stateProvider, $urlRouterProvider) {
-          $urlRouterProvider.otherwise("/");
+      .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+             $urlRouterProvider.otherwise("/");
 
-          $stateProvider
-              .state('friends', {
-                  url: "/",
-                  templateUrl: 'views/friends.html',
-                  controller: "FriendsCtrl"
-              });
+             $stateProvider
+                 .state("friends", {
+                     url: "/",
+                     templateUrl: "views/friends.html",
+                     controller: "FriendsCtrl"
+                 })
+                 .state("login", {
+                     url: "/login",
+                     template: "<h1>Login page</h1>"
+                 });
+
+             // use the HTML5 History API
+             $locationProvider.html5Mode(true).hashPrefix('!');
       });
 
-     return module;
+    return module;
  });
 
 
