cfa35eb3984df3bb679e9f8232ab7b5af808b485
Georgi Mateev <gmmateev92@abv.bg>
2014-05-14 08:37:19 +0300

    upgraded express to version 4.2.0

--- app.js
+++ app.js
@@ -4,16 +4,17 @@
  */
 
 var express = require('express');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var methodOverride = require('method-override');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var errorHandler = require('errorhandler');
+
 var api = require('./routes/api');
 var path = require('path');
 var graph = require('fbgraph');
 var Q = require('q');
-
-var service = require("./scripts/amazonService");
-var client = service.initialize("AKIAI7ALH67LC44E4CRQ", "BlYn/vpyKDWhRduBU27UhH902kW0WpIU4FGdX5ba", "associateTag");
-
-var suggestionsCore = require("./scripts/suggestions");
-var core = suggestionsCore.initialize(client, graph);
 
 var webSocketApi = require('./scripts/webSocketApi');
 
@@ -23,25 +24,15 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'www')));
-
-//Ensure that all url are redirected to index.html/#![path].
-//This will allow Angular to handle all url paths in Html5 mode and fallback to hashbang mode if needed.
-app.use(function(req, res){
-    return res.redirect(req.protocol + '://' + req.get('Host') + '/#!' + req.url);
-});
+app.use(favicon());
+app.use(logger('dev'));
+app.use(methodOverride());
+app.use(cookieParser('your secret here'));
+app.use(session());
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  app.use(errorHandler());
 }
 
 app.get("/api/facebook/friends", api.getFacebookFriends);
@@ -50,9 +41,17 @@ app.get('/api/facebook/search', api.facebookSearch);
 
 app.get('/api/amazon/similar', api.similarAmazonItems);
 
-var http = require('http')
-  , server = http.createServer(app)
-  , io = require('socket.io').listen(server)
+app.use(express.static(path.join(__dirname, 'www')));
+
+//Ensure that all url are redirected to index.html/#![path].
+//This will allow Angular to handle all url paths in Html5 mode and fallback to hashbang mode if needed.
+app.use(function(req, res){
+    return res.redirect(req.protocol + '://' + req.get('Host') + '/#!' + req.url);
+});
+
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 
 server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
