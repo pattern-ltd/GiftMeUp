d035d7dd27c39ce9780988b32b76818eed816251
Georgi Mateev <gmmateev92@abv.bg>
2014-05-31 23:14:30 +0300

    added loading spinner when fetching friends

--- www/scripts/controllers/friends.js
+++ www/scripts/controllers/friends.js
@@ -2,13 +2,18 @@ define(["controllers/module"], function (controllers) {
     controllers.controller("FriendsCtrl", ["$scope", "$rootScope", "$state", "$stateParams", "FacebookService", "WebSocketService",
         function ($scope, $rootScope, $state, $stateParams, facebookService, webSocketService) {
             $scope.getFriends = function () {
+                $scope.loadingFriends = true;
+
                 facebookService.getFriends()
                 .then(function (result) {
                     $scope.friends = result;
+                    $scope.loadingFriends = false;
                 });
             },
 
             $scope.searchFriends = function () {
+                $scope.loadingFriends = true;
+
                 var searchWords = $scope.searchFriendsKeywords;
 
                 var searchString = searchWords.replace(/\s/, '+');
@@ -16,6 +21,7 @@ define(["controllers/module"], function (controllers) {
                 facebookService.search(searchString, "user")
                 .then(function (result) {
                     $scope.friends = result;
+                    $scope.loadingFriends = false;
                 });
             }
 
