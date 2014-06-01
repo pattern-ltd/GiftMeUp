a6aab92fb2d7555fc4b8c2854c4028a8c9978a41
Georgi Mateev <gmmateev92@abv.bg>
2014-06-01 11:37:12 +0300

    style fixes, added tabs and search in the friends collapsible

---
+++ www/scripts/filters/loader.js
@@ -0,0 +1,4 @@
+define(["filters/search"], function(){
+    //This is empty file which requires all components in the filters module
+    //require this file to load the filter module with its filters
+});
